819ccc23d51069f9b8508fdbd335191ac02897ac
dariodiaz <dario.dad@gmail.com>
2020-08-15 19:09:15 -0300

    Adding a new validation for loading URLs & adding a new script to run specs

--- cypress/integration/course-examples/simple.spec.js
+++ cypress/integration/course-examples/simple.spec.js
@@ -2,5 +2,9 @@ describe('Browser actions', () => {
     it('should load correct url', () => {
         cy.visit('http://example.com', { timeout: 10000 })
     })
+
+    it('should check correct url', () => {
+        cy.url().should('include', 'example.com')
+    })
 })
 
