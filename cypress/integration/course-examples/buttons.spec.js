e30b7844622159a6cc6a07d1825707e45c1433f9
dariodiaz <dario.dad@gmail.com>
2020-08-17 17:53:57 -0300

    Counting elements on a page - Lecture 16

--- cypress/integration/course-examples/buttons.spec.js
+++ cypress/integration/course-examples/buttons.spec.js
@@ -1,13 +1,21 @@
 describe('Browser Actions - Buttons', () => {
     it('should load books website', () => {
         cy.visit('https://books.toscrape.com/index.html', { timeout: 10000})
-        cy.url().should('include', 'index.html')
+        cy.url()
+            .should('include', 'index.html')
     })
 
     it('should click on Travel Category', () => {
         cy.get('a')
             .contains('Travel')
             .click()
-        cy.get('h1').contains('Travel')
+        cy.get('h1')
+            .contains('Travel')
     })
-});
+
+    it('should displaye correct numbers of books', () => {
+        cy.get('.product_pod')
+            .its('length')
+            .should('eq', 12)
+    })
+})
