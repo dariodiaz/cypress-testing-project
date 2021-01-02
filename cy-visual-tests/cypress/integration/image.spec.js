a6d413248b3a00dd4e1feb157a6757929a47958a
dariodiaz <dario.dad@gmail.com>
2021-01-02 17:11:13 -0300

    Date & Time override for visual tests

--- cy-visual-tests/cypress/integration/image.spec.js
+++ cy-visual-tests/cypress/integration/image.spec.js
@@ -1,8 +1,15 @@
 const pages = ['http://example.com/']
 const sizes = ['iphone-6', 'ipad-2', [1200, 800]]
 describe('Visual Regression', () => {
-    it('My first visual regression test', () => {
-        cy.visit('http://example.com/')
-        cy.matchImageSnapshot()
+    sizes.forEach(size => {
+        pages.forEach(page => {
+            it(`Should match ${page} in resolution ${size}`, () => {
+                let currentTime = new Date(Date.UTC(2020, 1, 2)).getDate()
+                cy.clock(currentTime)
+                cy.setResolution(size)
+                cy.visit(page)
+                cy.matchImageSnapshot()
+            })
+        })
     })
 })
