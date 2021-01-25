21f2ebb1bacaaf94d1227364bfe3649ff5c5b97c
dariodiaz <dario.dad@gmail.com>
2021-01-25 16:49:23 -0300

    Setting up our first project with dashboard

--- cy-dashboard/cypress/integration/submit.spec.js
+++ cy-dashboard/cypress/integration/submit.spec.js
@@ -3,7 +3,7 @@ describe('Submit Form', () => {
         cy.visit('https://devexpress.github.io/testcafe/example')
         cy.get('#developer-name').type('Mike')
         cy.get('#tried-test-cafe').click()
-        cy.get('submit-button').click()
+        cy.get('#submit-button').click()
         cy.get('#article-header').contains('Thank you')
     })
 })
